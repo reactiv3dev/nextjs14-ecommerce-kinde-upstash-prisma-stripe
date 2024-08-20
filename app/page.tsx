f35aca683760ccd5578f74085582cbb587a49a5d
reactiv3dev <reactivedeveloper@protonmail.com>
2024-08-20 02:41:16 +0200

    kindeAuth added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
+ 
+
+ 
 
 export default function Home() {
   return (
     <div>
+      <Button asChild>
+        <LoginLink>Sign in</LoginLink>
+      </Button>
+      <Button asChild>
+        <RegisterLink>Sign up</RegisterLink>
+      </Button>
       <h1>hello from index page</h1>
     </div>
   );
